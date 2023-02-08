6f92b75ffcdf6f646f3ff1ecbb6bb94542cad9e8
Tushar Indurjeeth <tusharindurjeeth@protonmail.com>
2023-02-09 01:57:36 +0200

    Separated front-end

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/cms',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
